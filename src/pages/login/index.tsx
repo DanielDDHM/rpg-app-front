138309037ab293756c2faa714cfcc0f71969de79
DanielDDHM <daniel.ddhm@gmail.com>
2022-12-25 07:53:26 -0300

    chore:lint

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+const Login = () => {
+  const Login = 'Login Function Component!';
+
+  return (
+    <div className="App">
+      <h1>{Login}</h1>
+    </div>
+  );
+};
+
+export default Login;

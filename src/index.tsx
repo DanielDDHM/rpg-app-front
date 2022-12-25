013b983726ccc85f402fb6ab5399d55e01ab2a50
DanielDDHM <daniel.ddhm@gmail.com>
2022-12-25 09:07:17 -0300

    feat:initial-screen

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Login from './pages/login';
+import App from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Login />
+    <App />
   </React.StrictMode>,
 );
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -2,11 +2,29 @@ import React from 'react';
 import './index.css';
 
 const Login = () => {
-  const Login = 'Login Function Component!';
-
   return (
-    <div className="App">
-      <h1>{Login}</h1>
+    <div className="Auth-form-container">
+      <form className="Auth-form">
+        <div className="Auth-form-content">
+          <h3 className="Auth-form-title">Sign In</h3>
+          <div className="form-group mt-3">
+            <label>Email address</label>
+            <input type="email" className="form-control mt-1" placeholder="Enter email" />
+          </div>
+          <div className="form-group mt-3">
+            <label>Password</label>
+            <input type="password" className="form-control mt-1" placeholder="Enter password" />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </div>
+          <p className="text-center mt-2">
+            Forgot <a href="#">password?</a>
+          </p>
+        </div>
+      </form>
     </div>
   );
 };
